3f525dc9c2a7d717649c83cef63adf4fc9a01c39
BilalNaseer7773 <mbnaseer@mun.ca>
2024-11-03 19:18:42 -0330

    Push all the files to github

---
+++ Frontend/src/pages/home/Home.jsx
@@ -0,0 +1,18 @@
+import TopSellers from './TopSellers'
+import Banner from './Banner'
+import Recommended from './Recommended'
+import News from './News'
+
+const Home = () =>  {
+        return (
+            <>
+                <Banner/>
+                <TopSellers/>
+                <Recommended/>
+                <News/>
+            </>
+        )
+    }
+
+
+export default Home
